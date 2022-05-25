21bf30eb5abbc25d36b3a0a6db33cc0dd4ecde36
Shahbaz <fullstack.ali@gmail.com>
2022-05-25 10:45:05 +0530

    go to top button implimentation

--- src/app/Models/home/Home/home/home.component.ts
+++ src/app/Models/home/Home/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent implements OnInit {
 
+  
+  @HostListener("window:scroll", []) onWindowScroll() {
+    this.scrollFunction();
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        (document.getElementById("myBtn") as HTMLInputElement).style.display = "block";
+    } else {
+        (document.getElementById("myBtn") as HTMLInputElement).style.display = "none";
+    }
+}
+topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+} 
 
 }
